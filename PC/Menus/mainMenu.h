5f3149175cf9ec95ae3949fb694d912c8e66453b
ludogagnon <ludoabd@gmail.com>
2024-02-11 14:32:31 -0500

    update mainMenu.h

---
+++ PC/Menus/mainMenu.h
@@ -0,0 +1,26 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Menu {
+
+public:
+    void afficherMenu();
+    void menuPause();
+
+private:
+    int choix = 0;
+};
+
+
+
+
+
+
+
+
+
+#endif
