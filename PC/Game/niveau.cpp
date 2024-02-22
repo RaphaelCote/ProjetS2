2eb1f2bb46e1c8854bd7098e58f4d37795ac492a
jsnoreau <jsnoreau11@gmail.com>
2024-02-22 09:00:39 -0500

    clase niveau

--- PC/Game/niveau.cpp
+++ PC/Game/niveau.cpp
@@ -1,2 +1,35 @@
 #include "niveau.h"
 
+class niveau
+{
+private:
+    int nbraft;
+public:
+    niveau();
+    niveau(int);
+    ~niveau();
+    int hight=1080;
+    int wight=3000;
+
+    int backimge[1080*3000];
+    void creerListeRfat();
+
+};
+
+niveau::niveau()
+{
+    nbraft=2;
+    creerListeRfat();
+}
+niveau::niveau(int t){
+    nbraft=t;
+    creerListeRfat();
+}
+
+niveau::~niveau()
+{
+}
+
+void niveau::creerListeRfat(){
+    raft listeraft[nbraft];
+}
