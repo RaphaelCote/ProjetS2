2e331a648586a8d8778fcee277eee9a0badfd4a3
Ralmog <raphaelcote20@gmail.com>
2024-02-25 13:17:51 -0500

    save

--- PC/Game/niveau.cpp
+++ PC/Game/niveau.cpp
@@ -1,21 +1,25 @@
 #include "niveau.h"
-
-
+#include "playerCharacter.h"
+#include "enemyCharacter.h"
 
 Niveau::Niveau()
 {
-    nbraft=2;
-    creerListeRfat();
+    nbraft = 2;
+    creerListeRaft();
 }
-Niveau::Niveau(int t){
-    nbraft=t;
-    creerListeRfat();
+Niveau::Niveau(int t)
+{
+    nbraft = t;
+    creerListeRaft();
 }
 
 Niveau::~Niveau()
 {
 }
 
-void Niveau::creerListeRfat(){
-    //Boat listeraft[nbraft];
+void Niveau::creerListeRaft()
+{
+    characters.add(new PlayerCharacter(0, 100));
+
+    characters.add(new EnemyCharacter(2000, 100));
 }
--- PC/Game/niveau.h
+++ PC/Game/niveau.h
@@ -1,20 +1,24 @@
 #ifndef NIVEAU_H
 #define NIVEAU_H
 
+#include "../vecteur.h"
+#include "character.h"
+
 class Niveau
 {
 private:
     int nbraft;
+    Vecteur<Character *> characters;
+
 public:
     Niveau();
     Niveau(int);
     ~Niveau();
-    int hight=1080;
-    int wight=3000;
+    int hight = 1080;
+    int wight = 3000;
 
     int backimge[1];
-    void creerListeRfat();
-
+    void creerListeRaft();
 };
 
 #endif
