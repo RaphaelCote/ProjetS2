6c00ef785b0f1e3de323d4651376b61c7ff0b176
jsnoreau <jsnoreau11@gmail.com>
2024-02-29 11:02:19 -0500

    lib json + début classe gameloder

---
+++ PC/Game/gameloder.cpp
@@ -0,0 +1,16 @@
+#include "gameloder.h"
+#include <iostream>
+#include <fstream>
+#include <json.hpp>
+
+gameloder::gameloder(/* args */)
+{
+}
+
+gameloder::~gameloder()
+{
+}
+
+void gameloder::getlevelsformeJson(){
+
+}
---
+++ PC/Game/gameloder.h
@@ -0,0 +1,19 @@
+#ifndef GAMELODER_H
+
+#define GAMELODER_H
+
+class gameloder
+{
+private:
+ 
+public:
+    void getlevelsformeJson();
+    gameloder();
+    ~gameloder();
+
+};
+
+
+
+
+#endif
