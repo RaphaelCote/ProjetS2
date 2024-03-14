9e31e850fe6c476d2c29c5b0505bdd4b131a7a99
ludogagnon <ludoabd@gmail.com>
2024-03-14 15:05:27 -0400

    m

--- PC/Game/niveau.cpp
+++ PC/Game/niveau.cpp
@@ -73,12 +73,16 @@ void Niveau::MatRaft()
     
     for (int i = 0; playerBoats[i]!= NULL; i++)
     {
-        Couleur couleur[(playerBoats[i]->getHeight())/10][(playerBoats[i]->getWidth())/10];
+        //Pixels couleur[(playerBoats[i]->getHeight())/10][(playerBoats[i]->getWidth())/10];
+            Pixels **couleur = new Pixels*[(playerBoats[i]->getHeight())/10];
+            for (int i = 0; i < (playerBoats[i]->getHeight())/10; ++i) {
+            couleur[i] = new Pixels[playerBoats[i]->getWidth()/10];
+            }
 
 
         for (int i = 0; i < (playerBoats[i]->getHeight())/10; i++)
         {
-            for (int n = 0; i < (playerBoats[i]->getWidth())/10; n++)
+            for (int n = 0; n < (playerBoats[i]->getWidth())/10; n++)
             {
                 couleur[i][n].FrontColour = 6;      //gold
                 couleur[i][n].BackColour = 8;       //grey
@@ -87,16 +91,22 @@ void Niveau::MatRaft()
             
         }
      //AfficherEnBasGauche(couleur[][], x, y, width, hauteur)
+     delete[] couleur[i];
     }
+    
     
     for (int i = 0; enemyBoats[i]!= NULL; i++)
     {
-        Couleur couleur[(enemyBoats[i]->getHeight())/10][(enemyBoats[i]->getWidth())/10];
+        //Pixels couleur[(enemyBoats[i]->getHeight())/10][(enemyBoats[i]->getWidth())/10];
+        Pixels **couleur = new Pixels*[(enemyBoats[i]->getHeight())/10];
+            for (int i = 0; i < (enemyBoats[i]->getHeight())/10; ++i) {
+            couleur[i] = new Pixels[enemyBoats[i]->getWidth()/10];
+            }
 
 
         for (int i = 0; i < (enemyBoats[i]->getHeight())/10; i++)
         {
-            for (int n = 0; i < (enemyBoats[i]->getWidth())/10; n++)
+            for (int n = 0; n < (enemyBoats[i]->getWidth())/10; n++)
             {
                 couleur[i][n].FrontColour = 6;      //gold
                 couleur[i][n].BackColour = 8;       //grey
@@ -113,12 +123,16 @@ void Niveau::MatPlayer()
 {
     for (int v=0 ; playerBoats[v] != NULL ; v++)
     {
-        for (int i = 0;  playerBoats[v]->characters[i] != NULL; i++)
+        for (int f = 0;  playerBoats[v]->characters[f] != NULL; f++)
         {
-            Couleur MatCharacter[10][10];
-            for (int m = 0; i < 10; m++)
+            //Pixels MatCharacter[10][10];
+            Pixels **MatCharacter = new Pixels*[10];
+            for (int i = 0; i < 10; ++i) {
+            MatCharacter[i] = new Pixels[10];
+            }
+            for (int m = 0; m < 10; m++)
             {
-                for (int n = 0; i < 10; n++)
+                for (int n = 0; n < 10; n++)
                 {
                     MatCharacter[m][n].FrontColour = 16;      
                     MatCharacter[m][n].BackColour = 16;       //on remplie tt de transparent          
@@ -126,9 +140,9 @@ void Niveau::MatPlayer()
                 } 
             }
 
-            for (int m = 0; i < 3; m++)
+            for (int m = 0; m < 3; m++)
             {
-                for (int n = 5; i < 8; n++)
+                for (int n = 5; n < 8; n++)
                 {
                     MatCharacter[m][n].FrontColour = 10;      
                     MatCharacter[m][n].BackColour = 10;       //green       
@@ -142,11 +156,11 @@ void Niveau::MatPlayer()
             
             for (int i = 4; i < 6; i++)
             {
-                for (int t = 5; i < 10; i++)
+                for (int t = 5; t < 10; t++)
                 {
-                MatCharacter[4][i].FrontColour = 0;
-                MatCharacter[4][i].BackColour = 4; //gun          
-                MatCharacter[4][i].texture = '\xB1';
+                MatCharacter[i][t].FrontColour = 0;
+                MatCharacter[i][t].BackColour = 4; //gun          
+                MatCharacter[i][t].texture = '\xB1';
                 }
             }
 
@@ -175,6 +189,7 @@ void Niveau::MatPlayer()
             }
 
         }
+
     }
     
 
@@ -184,12 +199,18 @@ void Niveau::MatEnemy()
 {
     for (int v=0 ; enemyBoats[v] != NULL ; v++)
     {
-        for (int i = 0;  enemyBoats[v]->characters[i] != NULL; i++)
+        for (int f = 0;  enemyBoats[v]->characters[f] != NULL; f++)
         {
-            Couleur MatCharacter[10][10];
-            for (int m = 0; i < 10; m++)
+            //Pixels MatCharacter[10][10];
+            Pixels **MatCharacter = new Pixels*[10];
+            for (int i = 0; i < 10; ++i) {
+            MatCharacter[i] = new Pixels[10];
+            }
+
+
+            for (int m = 0; m < 10; m++)
             {
-                for (int n = 0; i < 10; n++)
+                for (int n = 0; n < 10; n++)
                 {
                     MatCharacter[m][n].FrontColour = 16;      
                     MatCharacter[m][n].BackColour = 16;       //on remplie tt de transparent          
@@ -197,9 +218,9 @@ void Niveau::MatEnemy()
                 } 
             }
 
-            for (int m = 0; i < 3; m++)
+            for (int m = 0; m < 3; m++)
             {
-                for (int n = 2; i < 5; n++)
+                for (int n = 2; n < 5; n++)
                 {
                     MatCharacter[m][n].FrontColour = 10;      
                     MatCharacter[m][n].BackColour = 10;       //green       
@@ -213,11 +234,11 @@ void Niveau::MatEnemy()
             
             for (int i = 4; i < 6; i++)
             {
-                for (int t = 0; i < 5; i++)
+                for (int t = 0; t < 5; t++)
                 {
-                MatCharacter[4][i].FrontColour = 0;
-                MatCharacter[4][i].BackColour = 4; //gun          
-                MatCharacter[4][i].texture = '\xB1';
+                MatCharacter[i][t].FrontColour = 0;
+                MatCharacter[i][t].BackColour = 4; //gun          
+                MatCharacter[i][t].texture = '\xB1';
                 }
             }
 
@@ -252,7 +273,11 @@ void Niveau::MatEnemy()
 
 void Niveau::MatWater()
 {
-    Couleur water[height/10][width/10];
+    //Pixels water[height/10][width/10];
+    Pixels **water = new Pixels*[height/10];
+    for (int i = 0; i < (height/10) ; ++i) {
+        water[i] = new Pixels[width/10];
+    }
     for (int i = 0; i < (height/10); i++)
     {
         for (int n = 0; n < (width/10); n++)
@@ -267,12 +292,135 @@ void Niveau::MatWater()
 
 void Niveau::MatBalle()
 {
+    //Pixels balle;
+    Pixels **balle = new Pixels*[1];
+    for (int i = 0; i < 1; ++i) {
+        balle[i] = new Pixels[1];
+    }
+    balle[0][0].FrontColour = 15; //white
+    balle[0][0].BackColour = 15;
+    balle[0][0].texture = ' ';
 }
 
 void Niveau::MatGrenade()
 {
+    //Pixels grenade;
+    Pixels **grenade = new Pixels*[1];
+    for (int i = 0; i < 1; ++i) {
+        grenade[i] = new Pixels[1];
+    }
+    grenade[0][0].FrontColour = 14 ; //yellow
+    grenade[0][0].BackColour = 14; 
+    grenade[0][0].texture = ' ';
 }
 
 void Niveau::MatRocket()
 {
+    Pixels **rocket = new Pixels*[4];
+    for (int i = 0; i < 4; ++i) {
+        rocket[i] = new Pixels[7];
+    }
+    for (int m = 0; m < 3; m++)
+    {
+        for (int n = 0; n < 6; n++)
+        {
+            rocket[m][n].FrontColour = 16;      
+            rocket[m][n].BackColour = 16;       //transparent          
+            rocket[m][n].texture = ' ';
+        } 
+    }
+    rocket[1][0].FrontColour = 12;      
+    rocket[1][0].BackColour = 12;       //lightred          
+    rocket[1][0].texture = ' ';
+
+    rocket[0][1].FrontColour = 4;      
+    rocket[0][1].BackColour = 4;       //red          
+    rocket[0][1].texture = ' ';
+
+    rocket[2][1].FrontColour = 4;      
+    rocket[2][1].BackColour = 4;       //red          
+    rocket[2][1].texture = ' ';
+
+    rocket[1][5].FrontColour = 4;      
+    rocket[1][5].BackColour = 4;       //red          
+    rocket[1][5].texture = ' ';
+
+    for (int i = 1; i < 5; i++)
+    {
+        rocket[1][i].FrontColour = 7;      
+        rocket[1][i].BackColour = 7;       //lightgrey       
+        rocket[1][i].texture = ' ';
+    }
+    //cons->AfficherEnBasGauche(rocket, , 10, 10, 30);
+
 }
+
+void Niveau::MatNuage()
+{
+    //Pixels nuage[4][9];
+    Pixels **nuage = new Pixels*[4];
+    for (int i = 0; i < 4; ++i) {
+        nuage[i] = new Pixels[9];
+    }
+    for (int m = 0; m < 4; m++)
+    {
+        for (int n = 0; n < 9; n++)
+        {
+            nuage[m][n].FrontColour = 16;      
+            nuage[m][n].BackColour = 16;       //transparent          
+            nuage[m][n].texture = ' ';
+        } 
+    }
+    for (int i = 2; i < 4; i++)
+    {
+        for (int n = 0; n < 9; n++)
+        {
+            nuage[i][n].FrontColour = 15;      
+            nuage[i][n].BackColour = 15;       //white          
+            nuage[i][n].texture = ' ';
+        }
+    }
+    for (int i = 1; i < 9; i++)
+    {
+        nuage[1][i].FrontColour = 15;      
+        nuage[1][i].BackColour = 15;          
+        nuage[1][i].texture = ' ';
+    }
+    for (int i = 3; i < 7; i++)
+    {
+        nuage[0][i].FrontColour = 15;      
+        nuage[0][i].BackColour = 15;          
+        nuage[0][i].texture = ' ';
+    }
+}
+
+/*
+Pixels *couleur = new Pixels[10];
+    for (int i = 0; i < 10; ++i) {
+        couleur[i] = new Pixels[30];
+    }
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int n = 0; n < 30; n++)
+            {
+                couleur[i][n].FrontColour = 6;      //gold
+                couleur[i][n].BackColour = 8;       //grey
+                couleur[i][n].texture = '\xB1';
+            }
+
+        }
+    cons->AfficherEnBasGauche(couleur, 10, 10, 10, 30);
+    
+    
+    
+    // Don't forget to deallocate memory
+    for (int i = 0; i < rows; ++i) {
+        delete[] myArray[i];
+    }
+    delete[] myArray;
+    
+    
+    
+    
+    */
