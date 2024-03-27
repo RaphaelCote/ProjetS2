6ef05b68b701978a702ed1575d0366c9e269e02d
vicsk8er <victor.larose@hotmail.com>
2024-03-26 20:39:06 -0400

    je fais visiblement qqch d'illégal

--- PC/Scenes/game.cpp
+++ PC/Scenes/game.cpp
@@ -221,12 +221,12 @@ void Game::PlayTurn()
         Projectile *enemyProjectile = ec->createEnemyProjectile();
 
         cout << "Angle : " << enemyProjectile->getAngleDegre() << " | Puissance : " << enemyProjectile->getPuissance() << endl;
-        Vecteur<Character*> players;
+        Vecteur<Character&> players;
         for(int i=0;i<activeLevel->enemyBoats.getSize();i++)
         {
             for(int j = 0; j < activeLevel->enemyBoats[i]->characters.getSize(); j++) 
             {
-                players.add(activeLevel->enemyBoats[i]->characters[j]);
+                players.add(*activeLevel->enemyBoats[i]->characters[j]);
             }
         }
         enemyProjectile->checkIfCharacterHit(players);
@@ -261,16 +261,17 @@ void Game::PlayerShoot()
     {
         return;
     }
-    Vecteur<Character*> enemies;
+    Vecteur<Character&> enemies;
     for(int i=0;i<activeLevel->enemyBoats.getSize();i++)
     {
         for(int j=0;j<activeLevel->enemyBoats[i]->characters.getCapacity();j++)
         {
-            enemies.add(activeLevel->enemyBoats[i]->characters[j]);
+            enemies.add(*activeLevel->enemyBoats[i]->characters[j]);
         }
     }
     activeLevel->enemyBoats[0]->characters[0];
-    projectile->checkIfCharacterHit(*(activeLevel->characters[1])); 
+    projectile->checkIfCharacterHit(enemies); 
+    //projectile->checkIfCharacterHit(*(activeLevel->characters[1])); 
     cout << "Le projectile a atteri a la position: (" << projectile->getBulletEndPosition().x;
     cout << ", " << projectile->getBulletEndPosition().y << ")" << endl;   
     // if (projectile->checkIfCharacterHit(*(level->characters[1])))
