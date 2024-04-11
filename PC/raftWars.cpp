39d85d7553d0de66ae46e6d16517fa8c9313e168
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-04-11 19:45:53 -0400

    come3

--- PC/raftWars.cpp
+++ PC/raftWars.cpp
@@ -222,7 +222,7 @@ int main(int argc, char *argv[])
 
     eventManager = new EventManager();
     //controls = new KeyboardControls(eventManager);
-    controls = new ControllerControls(eventManager, "COM4");
+    controls = new ControllerControls(eventManager, "COM3");
 
     gameWindow->AddContent(mainMenu);
     gameWindow->AddContent(gameWidget);
