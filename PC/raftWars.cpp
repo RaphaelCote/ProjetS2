418f2b2f9e3e1e4c96fa48ea5adc4e094bf42d6f
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-04-13 15:41:58 -0400

    okoko

--- PC/raftWars.cpp
+++ PC/raftWars.cpp
@@ -236,8 +236,8 @@ int main(int argc, char *argv[])
     thread.start();
 
     eventManager = new EventManager();
-    //controls = new KeyboardControls(eventManager);
-    controls = new ControllerControls(eventManager, "COM3");
+    controls = new KeyboardControls(eventManager);
+    //controls = new ControllerControls(eventManager, "COM3");
 
     gameWindow->AddContent(mainMenu);
     gameWindow->AddContent(gameWidget);
